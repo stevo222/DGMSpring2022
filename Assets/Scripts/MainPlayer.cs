494b20893df2d3af511c96c878df901a2e499650
stevo222 <stephenostler@yahoo.com>
2022-02-10 12:14:22 -0700

    Basic Syntax script and main player script

---
+++ Assets/Scripts/MainPlayer.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I am alive and my name is ");
+    }
+
+  
+}
