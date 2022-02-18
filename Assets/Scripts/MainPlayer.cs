70cac71af0feca90e9eee352dd4e99807f2b48a4
stevo222 <stephenostler@yahoo.com>
2022-02-17 17:25:03 -0700

    trying to work on movement variables for player

--- Assets/Scripts/MainPlayer.cs
+++ Assets/Scripts/MainPlayer.cs
@@ -4,11 +4,42 @@ using UnityEngine;
 
 public class MainPlayer : MonoBehaviour
 {
+    [SerializeField]
+    private float _speed = 3.5f;
+    [SerializeField]
+    private float _speedMultiplier = 2;
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("I am alive and my name is ");
+        transform.position = new Vector3(0, 0, 0);
     }
 
-  
+    void CalculateMovement()
+    {
+        //float horizontalInput = horizontalInput.GetAxis("Horizontal");
+        //float verticalInput = verticalInput.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(0, 0, 0);
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
+
+        if (transform.position.x >= 11.15f)
+
+        {
+            transform.position = new Vector3(-11.15f, transform.position.y, 0);
+        }
+
+        else if (transform.position.x <= -11.15f)
+
+        {
+            transform.position = new Vector3(11.15f, transform.position.y, 0);
+        }
+    }
+
+
+
+   
 }
