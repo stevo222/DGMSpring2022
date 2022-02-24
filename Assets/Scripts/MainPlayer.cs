01a468e813c0443d925a8aefcc848faf965a7060
stevo222 <stephenostler@yahoo.com>
2022-02-23 18:02:11 -0700

    Created move player script

--- Assets/Scripts/MainPlayer.cs
+++ Assets/Scripts/MainPlayer.cs
@@ -5,10 +5,7 @@ using UnityEngine;
 
 public class MainPlayer : MonoBehaviour
 {
-    [SerializeField]
-    private float _speed = 3.5f;
-    [SerializeField]
-    private float _speedMultiplier = 2;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -17,28 +14,6 @@ public class MainPlayer : MonoBehaviour
         transform.position = new Vector3(0, 0, 0);
     }
 
-    void CalculateMovement()
-    {
-        //float horizontalInput = horizontalInput.GetAxis("Horizontal");
-        //float verticalInput = verticalInput.GetAxis("Vertical");
-
-        Vector3 direction = new Vector3(0, 0, 0);
-        transform.Translate(direction * _speed * Time.deltaTime);
-
-        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
-
-        if (transform.position.x >= 11.15f)
-
-        {
-            transform.position = new Vector3(-11.15f, transform.position.y, 0);
-        }
-
-        else if (transform.position.x <= -11.15f)
-
-        {
-            transform.position = new Vector3(11.15f, transform.position.y, 0);
-        }
-    }
 
 
 
